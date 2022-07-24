73c658f56b14c5baa2f322cd9518facfa51a9bfc
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-07-24 19:11:38 -0300

    Att

--- config/plugins.js
+++ config/plugins.js
@@ -3,17 +3,3 @@ module.exports = () => {
         ckeditor: true
     }
 }
-
-module.exports = () => ({
-    graphql: {
-      enabled: true,
-      config: {
-        playgroundAlways: true,
-        defaultLimit: 10,
-        maxLimit: 20,
-        apolloServer: {
-          tracing: true,
-        },
-      }
-    }
-  })
