326766b614053b8aa1b8a558809bb11fb9f1e260
medeirospradoleonardo <medeiros.prado@unesp.br>
2022-07-25 15:19:54 -0300

    Fix Image

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,29 @@
+module.exports = [
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": [
+              "'self'",
+              "data:",
+              "blob:",
+              "*.amazonaws.com",
+            ],
+            "media-src": ["'self'", "data:", "blob:"],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::favicon",
+    "strapi::public",
+  ];
